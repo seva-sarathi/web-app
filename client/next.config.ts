431216b8ef0a6fdd1f1ffe2960dfe430953330ec
anuragchandr <nrgchandra@gmail.com>
2026-08-01 12:53:14 +0530

    feat: refactor Docker setup for client and server, enhance build stages, and remove Nginx configuration

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['10.208.141.181'],
+  output: 'standalone',
 };
 
 export default nextConfig;
